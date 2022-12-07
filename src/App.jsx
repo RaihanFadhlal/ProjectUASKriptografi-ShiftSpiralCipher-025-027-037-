c014e8cf5538ec13cc29fc823c5c7386c87a6185
RaihanFadhlal <raihanfadhlal@gmail.com>
2022-12-07 09:31:18 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { encrypt, decrypt, splitIntoChunk } from './function'
+import React from 'react'
 
 function App() {
   const [OutputText, setOutputText] = useState("");
@@ -22,7 +23,7 @@ function App() {
   };
 
   return (
-    <div className="container mt-5 p-3 prose rounded-md bg-green-200">
+    <div className="container mt-5 p-3 prose rounded-md bg-yellow-100">
       <h1 className="hero-content pt-0 m-0 text-center">Shift-Spiral Encoder/Decoder</h1>
       <div className="bg-white rounded-md">
         <h2 className="text-center pt-3 m-0">Input:</h2>
@@ -84,16 +85,16 @@ function App() {
           <div className="flex flex-row mb-5 p-2 gap-10 justify-center">
             <button
               type="submit"
-              className="btn btn-secondary w-32 rounded-full"
+              className="btn btn-secondary w-32 rounded-full bg-green-200"
             >
               Submit
             </button>
-            <button type="reset" onClick={() => {chooseMode('ENCRYPT')}} className="btn btn-error w-32 rounded-full">
+            <button type="reset" onClick={() => {chooseMode('ENCRYPT')}} className="btn btn-error w-32 rounded-full bg-red-200">
               Reset
             </button>
           </div>
         </form>
-        <h2>Hasil: {OutputText}</h2>
+        <h2 className="flex flex-row mb-5 p-2 gap-10 justify-center">Hasil: {OutputText}</h2>
       </div>
       {/* <KeyGeneration  /> */}
     </div>
